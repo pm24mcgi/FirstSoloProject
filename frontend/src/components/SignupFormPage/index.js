2e761a267e53348a7dcb4080ad14ea95617edcb4
pm24mcgi <pmcginn@highlinefm.com>
2022-05-27 12:28:33 -0400

    signup component styling

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -29,7 +29,7 @@ function SignupFormPage() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className='SignupForm'>
       <ul>
         {errors.map((error, idx) => <li key={idx}>{error}</li>)}
       </ul>
