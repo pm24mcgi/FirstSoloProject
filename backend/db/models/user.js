e07f3b123e1367c6fe0a63dd8026c4045a5a12cd
pm24mcgi <pmcginn@highlinefm.com>
2022-05-29 14:43:12 -0400

    User model association fix

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -51,7 +51,7 @@ module.exports = (sequelize, DataTypes) => {
 
   User.associate = function(models) {
     // associations can be defined here
-    User.hasMany(models.Tags, {foreignKey: 'userId'})
+    User.hasMany(models.Tag, {foreignKey: 'userId'})
     User.hasMany(models.Property, {foreignKey: 'userId'})
   };
 
