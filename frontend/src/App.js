94788175fbb52072ad504ae930e39f548fb10698
pm24mcgi <pmcginn@highlinefm.com>
2022-05-30 21:10:25 -0400

    landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
+import LandingPage from '../src/components/LandingPage'
 
 function App() {
   const dispatch = useDispatch();
@@ -15,8 +16,7 @@ function App() {
     <>
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
-        <Switch>
-        </Switch>
+        <LandingPage />
       )}
     </>
   );
