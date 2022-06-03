fda0cce9b42aa31f8890387d634cff1a455a7451
pm24mcgi <pmcginn@highlinefm.com>
2022-06-03 18:08:19 -0400

    first

--- frontend/src/components/EditNote/index.js
+++ frontend/src/components/EditNote/index.js
@@ -6,34 +6,32 @@ import './EditNote.css'
 
 
 const EditNote = ({note, setEditOpen}) => {
-  // const sessionUser = useSelector(state => state.session.user);
-  // const propObj = useSelector(state => state.properties)
-  // const dispatch = useDispatch();
-  // const history = useHistory();
+  const history = useHistory();
+  const propertyId = note.propertyId;
+  const id = note.id;
 
-  // const [description, setDescription] = useState('');
-  // const [body, setBody] = useState('');
+  const [description, setDescription] = useState('');
+  const [body, setBody] = useState('');
 
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  //   const payload = {
-  //     description,
-  //     body,
-  //     propertyId
-  //   }
+    const payload = {
+      id,
+      description,
+      body,
+      propertyId
+    }
+    await dispatch(editNote(id, payload))
+        .then(() => history.push('/properties'))
+  }
 
-  //   await dispatch(editNote(id, payload))
-  //       .then(() => history.push('/properties'))
+  return (
+    <>
 
-    return (
-      <>
-        Hello From Edit Form
-        <button onClick={() => {setEditOpen(false)}}>Cancel</button>
-
-      </>
-    )
+      <button onClick={() => {setEditOpen(false)}}>Cancel</button>
+    </>
+  )
 }
-
 
 export default EditNote
